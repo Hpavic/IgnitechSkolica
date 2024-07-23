b94dbff9c3c33d2722594318b97ac79286f2df98
Hpavic <hrvoje.pavic994@gmail.com>
2024-07-23 17:52:09 +0200

    nullable warning GradeText

--- IgnitechSkolica/Models/Grade.cs
+++ IgnitechSkolica/Models/Grade.cs
@@ -14,7 +14,7 @@
         public int Id { get; set; }
         public int Value { get; set; }
         public DateTime CreatedOn { get; set; }
-        public string GradeText { get; set; }
+        public string? GradeText { get; set; }
         public int GradeNumber { get; set; }
     }
 }
